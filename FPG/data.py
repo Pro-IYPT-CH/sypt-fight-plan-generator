e9de5caf395105c32d9a73cb06af41a10a619b36
AedanChristie <aedan@thechristies.ch>
2016-06-19 10:59:40 +0200

    Initial push
    Some project files and config

---
+++ FPG/data.py
@@ -0,0 +1,20 @@
+#
+#
+#
+#
+
+class Tournament:
+	"""A class representing a tournament"""
+    def __init__(self):
+    	return self
+
+
+class Round:
+    def __init__(self):
+    	return self
+
+
+class Fight:
+    def __init__(self):
+    	return self
+
